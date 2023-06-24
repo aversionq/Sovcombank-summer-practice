b22a23459731be69dc5ef679a52afb9a97e83bc7
DESKTOP-NEMIBMN\arskz <arskzxc@gmail.com>
2023-06-24 17:08:08 +0400

    Add BLL DTOs

---
+++ TodoList/TodoList.BLL.DTOs/TodoItemDTO.cs
@@ -0,0 +1,13 @@
+﻿using TodoList.Entities;
+
+namespace TodoList.BLL.DTOs
+{
+    public class TodoItemDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Priority { get; set; }
+        public string Text { get; set; }
+        public bool IsDone { get; set; } = false;
+    }
+}
