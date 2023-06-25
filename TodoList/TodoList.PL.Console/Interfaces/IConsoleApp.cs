8fc4e4039daf018192686665f94cd2a1c74c8eea
DESKTOP-NEMIBMN\arskz <arskzxc@gmail.com>
2023-06-25 22:39:21 +0400

    Add setup logic

---
+++ TodoList/TodoList.PL.Console/Interfaces/IConsoleApp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.PL.Console.Interfaces
+{
+    public interface IConsoleApp
+    {
+        public Task Run();
+    }
+}
