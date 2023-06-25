fe20a40a5b72022c4616aa176fe6f956ee0cc204
DESKTOP-NEMIBMN\arskz <arskzxc@gmail.com>
2023-06-25 22:40:01 +0400

    Add dependency injection & program start

--- TodoList/TodoList.PL.Console/Program.cs
+++ TodoList/TodoList.PL.Console/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace TodoList.PL.Console
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.ComponentModel.Design;
+using TodoList.BLL.Implementations;
+using TodoList.BLL.Interfaces;
+using TodoList.DAL.Interfaces;
+using TodoList.DAL.JsonDAO;
+using TodoList.PL.Console.Implementations;
+using TodoList.PL.Console.Interfaces;
+
+namespace TodoList.PL.Console
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            // Использование встроенного внедрения зависимостей.
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddSingleton<ITodoListDAO, JsonDAO>();
+                    services.AddSingleton<ITodoListBLL, TodoListBusiness>();
+                    services.AddSingleton<IConsoleUI, ConsoleUI>();
+                    services.AddSingleton<IConsoleHelper, ConsoleHelper>();
+                    services.AddSingleton<IConsoleApp, ConsoleApp>();
+                })
+                .Build();
 
+            var consoleApp = ActivatorUtilities.CreateInstance<ConsoleApp>(host.Services);
+            await consoleApp.Run();
         }
     }
 }
